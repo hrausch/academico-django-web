b398db8a5269962eae0daeaa805f8ef26b973b88
Herbert <hrausch@gmail.com>
2025-02-14 10:18:56 -0300

    adicionando a landing page do projeto

--- professorhub/landing_page/views.py
+++ professorhub/landing_page/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+
+#view executada quando o formulário de cadastrado é submetido
+def submit_register(request):
+    if request.method == 'POST':
+        nome = request.POST.get('registerName')
+        pwd = request.POST.get('registerPassword')
+        correio_eletronico = request.POST.get('registerEmail')
+
+        print(f"Nome: {nome}, Email: {correio_eletronico}, Senha: {pwd}")
+
+        if nome and pwd and correio_eletronico:
+            message = f"Nome: {nome}, Email: {correio_eletronico}, Senha: {pwd}"
+            return HttpResponse(message)
+        else:
+            return HttpResponse("Algum campo está vazio.")
+    else:
+        return HttpResponse("Erro ao processar.")
